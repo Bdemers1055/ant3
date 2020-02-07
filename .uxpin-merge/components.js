78de96fe1f1c11ea69fe96e3c97ca3c21fee6bc2
bdemers1055 <bdemers1055@gmail.com>
2020-02-07 13:41:37 -0500

    uxpin circlecli

---
+++ .uxpin-merge/components.js
@@ -0,0 +1,124 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Icon from '../src/Icon/Icon';
+import Typography from '../src/Typography/Typography';
+import Button from '../src/Button/Button';
+import Fab from '../src/Fab/Fab';
+import IconButton from '../src/IconButton/IconButton';
+import Link from '../src/Link/Link';
+import AppBar from '../src/AppBar/AppBar';
+import BottomNavigation from '../src/BottomNavigation/BottomNavigation';
+import BottomNavigationAction from '../src/BottomNavigationAction/BottomNavigationAction';
+import Drawer from '../src/Drawer/Drawer';
+import Tab from '../src/Tab/Tab';
+import Tabs from '../src/Tabs/Tabs';
+import TabsContainer from '../src/TabsContainer/TabsContainer';
+import Toolbar from '../src/Toolbar/Toolbar';
+import TabContent from '../src/TabsContainer/TabContent/TabContent';
+import Grid from '../src/Grid/Grid';
+import GridList from '../src/GridList/GridList';
+import GridListTile from '../src/GridListTile/GridListTile';
+import GridListTileBar from '../src/GridListTileBar/GridListTileBar';
+import Card from '../src/Card/Card';
+import CardActions from '../src/CardActions/CardActions';
+import CardContent from '../src/CardContent/CardContent';
+import CardHeader from '../src/CardHeader/CardHeader';
+import CardMedia from '../src/CardMedia/CardMedia';
+import Checkbox from '../src/Checkbox/Checkbox';
+import FilledInput from '../src/FilledInput/FilledInput';
+import FormControl from '../src/FormControl/FormControl';
+import FormControlLabel from '../src/FormControlLabel/FormControlLabel';
+import FormGroup from '../src/FormGroup/FormGroup';
+import FormHelperText from '../src/FormHelperText/FormHelperText';
+import FormLabel from '../src/FormLabel/FormLabel';
+import Input from '../src/Input/Input';
+import InputLabel from '../src/InputLabel/InputLabel';
+import MenuItem from '../src/MenuItem/MenuItem';
+import MenuList from '../src/MenuList/MenuList';
+import OutlinedInput from '../src/OutlinedInput/OutlinedInput';
+import Radio from '../src/Radio/Radio';
+import RadioGroup from '../src/RadioGroup/RadioGroup';
+import Select from '../src/Select/Select';
+import Switch from '../src/Switch/Switch';
+import TextField from '../src/TextField/TextField';
+import Avatar from '../src/Avatar/Avatar';
+import Backdrop from '../src/Backdrop/Backdrop';
+import Badge from '../src/Badge/Badge';
+import Chip from '../src/Chip/Chip';
+import Collapse from '../src/Collapse/Collapse';
+import Paper from '../src/Paper/Paper';
+import List from '../src/List/List';
+import ListItem from '../src/ListItem/ListItem';
+import ListItemAvatar from '../src/ListItemAvatar/ListItemAvatar';
+import ListItemIcon from '../src/ListItemIcon/ListItemIcon';
+import ListItemSecondaryAction from '../src/ListItemSecondaryAction/ListItemSecondaryAction';
+import ListItemText from '../src/ListItemText/ListItemText';
+import ListSubheader from '../src/ListSubheader/ListSubheader';
+import CircularProgress from '../src/CircularProgress/CircularProgress';
+import LinearProgress from '../src/LinearProgress/LinearProgress';
+import CssBaseline from '../src/CssBaseline/CssBaseline';
+import Image from '../src/Image/Image';
+import Wrapper from '../src/Wrapper/UXPinWrapper.js';
+export {
+  Icon,
+  Typography,
+  Button,
+  Fab,
+  IconButton,
+  Link,
+  AppBar,
+  BottomNavigation,
+  BottomNavigationAction,
+  Drawer,
+  Tab,
+  Tabs,
+  TabsContainer,
+  Toolbar,
+  TabContent,
+  Grid,
+  GridList,
+  GridListTile,
+  GridListTileBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Checkbox,
+  FilledInput,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormHelperText,
+  FormLabel,
+  Input,
+  InputLabel,
+  MenuItem,
+  MenuList,
+  OutlinedInput,
+  Radio,
+  RadioGroup,
+  Select,
+  Switch,
+  TextField,
+  Avatar,
+  Backdrop,
+  Badge,
+  Chip,
+  Collapse,
+  Paper,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemIcon,
+  ListItemSecondaryAction,
+  ListItemText,
+  ListSubheader,
+  CircularProgress,
+  LinearProgress,
+  CssBaseline,
+  Image,
+  Wrapper,
+  React,
+  ReactDOM,
+};
---
+++ uxpin.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  components: {
+    categories: [
+      {
+        name: "General",
+        include: ["src/Icon/Icon.js", "src/Typography/Typography.js"]
+      },
+      {
+        name: "Actions",
+        include: [
+          "src/components/Authorized.jsx"
+        ]
+      }
+    ]
+  },
+  name: "Antd Design 3"
+};
+
+/**
+ * Currently not supported:
+ * 'src/Menu/Menu.js',
+ * 'src/ExpansionPanel/ExpansionPanel.js',
+ * 'src/ExpansionPanelActions/ExpansionPanelActions.js',
+ * 'src/ExpansionPanelDetails/ExpansionPanelDetails.js',
+ * 'src/ExpansionPanelSummary/ExpansionPanelSummary.js'
+ * 'src/Snackbar/Snackbar.js'
+ * 'src/SnackbarContent/SnackbarContent.js'
+ */
